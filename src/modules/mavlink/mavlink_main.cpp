a83e87b35442de0aeba0291ac2a295455ad98537
JBuzzatto <joaosansao@gmail.com>
2023-08-22 15:13:34 +1200

    added servo raw commands to be sent over wifi

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1780,6 +1780,8 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("RC_CHANNELS", 0.5f);
 		configure_stream_local("SYS_STATUS", 0.1f);
 		configure_stream_local("VFR_HUD", 1.0f);
+		configure_stream_local("SERVO_OUTPUT_RAW_0", 5.0f);
+		// configure_stream_local("SERVO_OUTPUT_RAW_1", 5.0f);
 
 #if !defined(CONSTRAINED_FLASH)
 		configure_stream_local("LINK_NODE_STATUS", 1.0f);
