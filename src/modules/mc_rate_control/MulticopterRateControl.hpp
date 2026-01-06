58a636317ff86a37d6ae3f1fac29b0f3221a9239
bo75salim <bo75salim@hotmail.com>
2026-01-06 17:21:42 +1300

    Disable Anti-Windup

--- src/modules/mc_rate_control/MulticopterRateControl.hpp
+++ src/modules/mc_rate_control/MulticopterRateControl.hpp
@@ -166,7 +166,9 @@ private:
 		(ParamFloat<px4::params::MC_ACRO_SUPEXPO>) _param_mc_acro_supexpo,		/**< superexpo stick curve shape (roll & pitch) */
 		(ParamFloat<px4::params::MC_ACRO_SUPEXPOY>) _param_mc_acro_supexpoy,		/**< superexpo stick curve shape (yaw) */
 
-		(ParamBool<px4::params::MC_BAT_SCALE_EN>) _param_mc_bat_scale_en
+		(ParamBool<px4::params::MC_BAT_SCALE_EN>) _param_mc_bat_scale_en,
+		(ParamInt<px4::params::DTRG_CSV_MIXER>) _csv_mixer,
+		(ParamInt<px4::params::DTRG_WINDUP_EN>) _param_antiwindup_en
 
 	)
 };
